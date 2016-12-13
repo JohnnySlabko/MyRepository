3952a71b0b7696e1651caf9d5e4d9ef8eb6f228a
JohnnySlabko <slabko96@mail.ua>
2016-12-14 00:59:05 +0300

    Изменили начинку

--- Class5.java
+++ Class5.java
@@ -1,10 +1,1 @@
-public class Class5 {
-    public static void main(String[] args){
-        TArray string = new TArray();
-        string.Show_task();
-        string.Initialization();
-        string.Show_array();
-        string.Solution();
-    }
-
-}
+print("Hello world");
