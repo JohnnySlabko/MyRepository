4bdee5b86c687766bcd93a4cae473119f9b133b5
JohnnySlabko <slabko96@mail.ua>
2016-12-14 00:52:44 +0300

    5 лаба

---
+++ Class5.java
@@ -0,0 +1,10 @@
+public class Class5 {
+    public static void main(String[] args){
+        TArray string = new TArray();
+        string.Show_task();
+        string.Initialization();
+        string.Show_array();
+        string.Solution();
+    }
+
+}
